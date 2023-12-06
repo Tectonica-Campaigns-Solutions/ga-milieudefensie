3145c32dcc480d29010c9981158755b6deec6d49
Rodrigo Leon <rodrigo@tectonica.co>
2023-12-06 16:04:31 -0300

    Hero home fixes

--- src/components/Global/HomeHero/HomeHero.js
+++ src/components/Global/HomeHero/HomeHero.js
@@ -5,15 +5,17 @@ function HomeHero({ title, subtitle, image }) {
   const heroBgImage = image?.gatsbyImageData?.images?.fallback?.src;
 
   return (
-    <div className="hero-home" style={{ backgroundImage: `url(${heroBgImage})` }}>
-      <div className="content">
-        {title && <h1>{title}</h1>}
-        {subtitle && <div className="introduction" dangerouslySetInnerHTML={{ __html: subtitle }} />}
+    <div className="wrapper-hero">
+      <div className="hero-home" style={{ backgroundImage: `url(${heroBgImage})` }}>
+        <div className="content">
+          {title && <h1>{title}</h1>}
+          {subtitle && <div className="introduction" dangerouslySetInnerHTML={{ __html: subtitle }} />}
 
-        {/* Form here */}
+          {/* Form here */}
+        </div>
+
+        <div className="overlay" />
       </div>
-
-      <div className="overlay" />
     </div>
   );
 }
